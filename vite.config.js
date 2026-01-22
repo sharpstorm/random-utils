aed935427cb207792b78a8b04a7e772ebcb0858a
sharpstorm <chimchar17@gmail.com>
2026-01-22 16:39:57 +0800

    feat: add ble app

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,22 @@
 // vite.config.js
-import { resolve } from 'path';
-import { nodePolyfills } from 'vite-plugin-node-polyfills';
+import { resolve } from "path";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 module.exports = {
   build: {
     rollupOptions: {
       input: {
-        brotliInflator: resolve(__dirname, 'brotli-inflator/index.html')
-      }
-    }
+        brotliInflator: resolve(__dirname, "brotli-inflator/index.html"),
+        nfcTerminal: resolve(__dirname, "nfc-terminal/index.html"),
+      },
+    },
   },
-  base: './',
+  base: "./",
   plugins: [
     nodePolyfills({
       globals: {
-        Buffer: true
-      }
+        Buffer: true,
+      },
     }),
   ],
-}
+};
